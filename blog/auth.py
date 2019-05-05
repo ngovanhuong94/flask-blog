cdb545456caa6ac6c744cc1d743ffb5ae1544b65
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-05-05 12:08:52 +0300

    register page

--- blog/auth.py
+++ blog/auth.py
@@ -1,7 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .db import get_db
+from werkzeug.security import generate_password_hash
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
+    db = get_db()
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        password2 = request.form['password2']   
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+        elif not password2:
+            error = 'Confirm password is required'
+        elif password != password2:
+            error = 'Passwords must matchs'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'Username was used'
+        
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password),)
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+        flash(error)
+
     return render_template('register.html')
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    return 'Login page'
