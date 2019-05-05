9b7102a30553912c4bdddb7e6ec8624413aa3810
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-05-05 12:29:13 +0300

    login page

--- blog/auth.py
+++ blog/auth.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from .db import get_db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -38,4 +38,33 @@ def register():
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'Login page'
+    db = get_db()
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # validation
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+        # fetch user by username
+        user = db.execute(
+            'SELECT id, username, password FROM user WHERE username=?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Username not found'
+        # check password
+        elif not check_password_hash(user['password'], password):
+            error = 'Password incorrect'
+        
+        
+        if error is None:
+            #save user id to session
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('hello_world'))
+        # show error to client
+        flash(error)
+    return render_template('login.html')
