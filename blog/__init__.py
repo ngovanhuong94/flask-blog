9f753b9c8ee9a58add5cc53ffc8de32bb5aba866
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-05-07 17:09:04 +0300

    edit post

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
+
 
 def create_app():
     # create app
@@ -29,4 +30,12 @@ def create_app():
     from . import db
     db.init_app(app)
 
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(403)
+    def forbidden(e):
+        return render_template('403.html'), 403
+    
     return app
--- blog/blog.py
+++ blog/blog.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, g, redirect, url_for
+from flask import Blueprint, render_template, request, g, redirect, url_for, abort
 
 from .auth import login_required
 from .db import get_db
@@ -36,3 +36,52 @@ def create():
             return redirect(url_for('blog.index'))
         flash(error)
     return render_template('create.html')
+
+
+def get_post(id, check_author=True):
+    db = get_db()
+
+    post = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id=u.id'
+        ' WHERE p.id=?', (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404)
+    
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    return post
+
+
+@blog_bp.route('/<int:id>')
+def detail(id):
+    post = get_post(id, False)
+    return render_template('detail.html', post=post)
+
+
+@blog_bp.route('/<int:id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = get_post(id)
+    error = None
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        db = get_db()
+        if not title:
+            error = 'Title is required'
+        elif not body:
+            error = 'Body is required'
+        
+        if error is None:
+            db.execute(
+                'UPDATE post'
+                ' SET title=?, body=?'
+                ' WHERE id=?', (title, body, post['id'],)
+            )
+            db.commit()
+            return redirect(url_for('blog.detail', id=post['id']))
+        flash(error)
+    return render_template('edit.html', post=post)
